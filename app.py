69b70fae19d4cd4abfa1e7e9c0ce7ec881a2176a
navneet-cerecode <cerecode9@gmail.com>
2026-09-06 15:17:11 +0530

    Model changed

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def initialize_models():
     
     llm = ChatGroq(
         temperature=0, 
-        model_name="llama-3.1-8b-instant",  
+        model_name="openai/gpt-oss-20b",  
         groq_api_key=st.secrets["GROQ_API_KEY"]
     )
     
